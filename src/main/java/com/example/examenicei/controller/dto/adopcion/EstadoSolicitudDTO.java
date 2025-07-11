8903ea0567a12ab49993f0ec4684fd648c31c13c
PC-VICENTE\PCV <jvicentecv@gmail.com>
2025-07-10 21:03:20 -0400

    create dto EstadoSolicitudDTO

---
+++ src/main/java/com/example/examenicei/controller/dto/adopcion/EstadoSolicitudDTO.java
@@ -0,0 +1,13 @@
+package com.example.examenicei.controller.dto.adopcion;
+
+public class EstadoSolicitudDTO {
+    private String estado;
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
+
